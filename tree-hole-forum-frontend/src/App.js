c3f9c8aa2dbd7d8242a1ce2626981c6e5fb78ad8
zehuap <zehuap1@uci.edu>
2020-12-06 16:04:55 +0800

    Modify main class

--- tree-hole-forum-frontend/src/App.js
+++ tree-hole-forum-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from 'antd';
 import './App.css';
 
+//This is an unused class
 const App = () => (
     <div className="App">
       <Button type="primary">Button</Button>
